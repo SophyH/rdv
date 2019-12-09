6e0bf5ff47b7572cbcd0e96fdeb676a4f23d95bb
SophyH <so.huk@laposte.net>
2019-12-09 16:49:11 +0100

    personnerepository

--- src/main/java/rdv/repository/PersonneRepository.java
+++ src/main/java/rdv/repository/PersonneRepository.java
@@ -1,5 +1,23 @@
 package rdv.repository;
 
-public interface PersonneRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import rdv.model.Personne;
+import rdv.model.Praticien;
+
+public interface PersonneRepository extends JpaRepository<Personne, Integer> {
+
+	@Query("select distinct p from Praticien p where p.nom = :nom")
+	public Optional<Praticien> findByNom(String nom);
+
+	@Query("select distinct p from Praticien p left join fetch p.adresses left join fetch p.specialites")
+	public Optional<Praticien> findWithAdressesAndSpecialites();
+
+	@Query("select distinct p from Praticien p left foin fetch p.adresses.ville where p.adresses.ville = :ville")
+	public List<Praticien> findAllByVille(String ville);
 
 }
