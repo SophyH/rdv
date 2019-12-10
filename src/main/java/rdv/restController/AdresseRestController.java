412c2f3d32aef5ed7a329e57471d2d4670ef1e76
sarahkaddah <sarah.kaddah@hotmail.fr>
2019-12-10 17:35:32 +0100

    lala

--- src/main/java/rdv/restController/AdresseRestController.java
+++ src/main/java/rdv/restController/AdresseRestController.java
@@ -102,7 +102,7 @@ public class AdresseRestController {
 //	}
 
 //	@JsonView(JsonViews.AdresseWithPraticien.class)
-//	@GetMapping("/{nompraticien}")
-//	
+//	@GetMapping("/{nompraticien}"){
+//	}
 	
 }
