8d5c825dbc862b32f8572e588ca124b6e2bd3ef3
sarahkaddah <sarah.kaddah@hotmail.fr>
2019-12-10 12:04:55 +0100

    query login et userRole

--- src/main/java/rdv/repository/LoginRepository.java
+++ src/main/java/rdv/repository/LoginRepository.java
@@ -1,11 +1,17 @@
 package rdv.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import rdv.model.Login;
 
 public interface LoginRepository extends JpaRepository<Login, String> {
 
-//	@Query("select l from Login l left join fetch l.role where l.username=:username")
-//	Optional<Login> findByUserNameWithUserRole(String username);
+	// Je veux chercher le role (admin/patient/praticien) en fonction du username
+	@Query("select l from Login l left join fetch l.role where l.username=:username")
+	Optional<Login> findByUserNameWithUserRole(@Param("username") String username);
+
 }
